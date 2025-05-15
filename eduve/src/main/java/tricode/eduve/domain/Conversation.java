e6a0f9d086545a6917bb49e261add16a5f7b4db0
hyejin2234 <144921254+hyejin2234@users.noreply.github.com>
2025-05-15 22:01:20 +0900

    fix

--- eduve/src/main/java/tricode/eduve/domain/Conversation.java
+++ eduve/src/main/java/tricode/eduve/domain/Conversation.java
@@ -36,7 +36,7 @@ public class Conversation extends CreatedTimeEntity {
     private String lastTopic;
 
     @Column(nullable = false)
-    private LocalDateTime updatedTime; //메시지 생성 시간
+    private LocalDateTime updatedTime; 
 
 
     public Conversation(String conversationName, User user) {
